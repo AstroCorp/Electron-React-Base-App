e06f730dd2814ed76f772c56a9c95f78ea8bfdb5
Alejandro Leal <aleaberm12@gmail.com>
2020-01-28 10:33:50 +0100

    resolved #19

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from './store';
+import App from './containers/app.js';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const target = document.querySelector('#root');
+
+render(
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
+	</Provider>,
+	target
+);
