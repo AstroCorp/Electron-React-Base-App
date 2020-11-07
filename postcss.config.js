5ec206bbb2fca4863d5285fd3c7a0e66ca3b9558
Alejandro Leal <aleaberm12@gmail.com>
2020-11-07 01:54:15 +0100

    Dependencias actualizadas

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-const TailwindVueExtractor = content => {
+const TailwindExtractor = content => {
     const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
     return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_:/]+/g) || [];
 };
@@ -11,7 +11,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     css: [`./src/**/*.@(${extensionsOfCSS.join("|")})`],
     extractors: [
         {
-            extractor: TailwindVueExtractor,
+            extractor: TailwindExtractor,
             extensions: extensionsUsingCSS,
         },
     ],
